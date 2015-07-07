69e7f27f65f8ef2059d288c23f39affb599b508b
laysejuju <laysejuju@gmail.com>
2015-07-07 12:48:18 -0300

    teste2

--- input.js
+++ input.js
@@ -1,15 +1,16 @@
+
 function inicializar(){
 	var tela = document.getElementById("tela");
 	var c = tela.getContext("2d");	
 			
 			c.fillStyle="#00ff00"; 
-			c.fillRect(340,200,200,400); 
+			c.fillRect(0,0,200,400); 
 			c.fillStyle="#ff0000";
-			c.fillRect(740,200,200,400);
+			c.fillRect(400,0,200,400);
 			c.fillStyle="gray";
-			
+		
 			c.fillStyle="blue";
 			c.beginPath();
-			c.arc(640,400,50,0 * Math.PI,2*Math.PI);
+			c.arc(300,200,50,0 * Math.PI,2*Math.PI);
 			c.fill();			
 }
