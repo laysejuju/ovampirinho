17913a9ed9b7e9ae55bf6aba88996a8ad6c4a773
laysejuju <laysejuju@gmail.com>
2015-07-08 15:09:24 -0300

    ATUALIZAÇÃO

--- input.js
+++ input.js
@@ -1,21 +1,89 @@
+var jogadorPosicaoX, jogadorAltura, jogadorLargura, velocidadeJogador;
+
+
+jogadorAltura= 19;
+jogadorLargura= 14;
+
+var posVerdeX = 10;
+var posVerdeY = 0;
+
+
 function inicializar(){	
+
+	document.getElementById('menu').style.display='none';
 	var tela = document.getElementById("tela");
 	var c = tela.getContext("2d");	
 	
-		
-			c.fillStyle="#00ff00"; 
-			c.fillRect(0,0,200,400); 
-			c.fillStyle="#ff0000";
-			c.fillRect(400,0,200,400);
-			c.fillStyle="gray";
-		
-			c.fillStyle="blue";
-			c.beginPath();
-			c.arc(300,200,50,0 * Math.PI,2*Math.PI);
-			c.fill();	
+	jogadorPosicaoX = 30;
+	velocidadeJogador=25;
 
-			document.body.appendChild(tela);
+	// Jogador
+			var imagem = new Image();
+			imagem.src = 'image/vampiro.png';
+			
+			imagem.onload = function(){
+				c.drawImage(imagem,jogadorPosicaoX,(tela.height - jogadorAltura)-35,jogadorLargura,jogadorAltura);
+			}
+		
+		
+		//chão
+			c.fillStyle="#00ff00";
+			c.fillRect(0, 115, 300, 5);
+			c.fill();
+				
+			c.fillStyle="#0B8C32";
+			c.fillRect(0, 120,  300, 30);
+			c.fill();
+	
+			
+		//comandos do teclado	
+			function keyDown(e) 
+			{
+				if(e.keyCode == 37)
+				{
+				console.log('keydown 37');
+					if(jogadorPosicaoX > 0)
+					{
+						jogadorPosicaoX -= 25;
+						
+					}
+				}
+				
+				if(e.keyCode == 39)
+				{
+				console.log('keydown 39');
+					if(jogadorPosicaoX < (tela.width - jogadorLargura))
+					{
+						jogadorPosicaoX += 25;
+						console.log('keydown 39');
+						
+					}
+				}
+			}
+			
+			
+	
+			
+			tela.addEventListener('keydown', keyDown);
+			console.log(tela.width);
+			console.log('keydown');
+			
 }
 
 
 
+
+			function onLeft(){
+				
+				jogadorPosicaoX -= 25;
+					
+				console.log("Left!");
+			}
+
+			function onRight(){
+				
+				jogadorPosicaoX +=25;
+
+				console.log("Right!");
+			}
+	
