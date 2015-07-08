451911165e68ad88d338d6010010031899dac740
laysejuju <laysejuju@gmail.com>
2015-07-07 21:59:41 -0300

    Atualização

--- input.js
+++ input.js
@@ -1,7 +1,8 @@
-function inicializar(){
+function inicializar(){	
 	var tela = document.getElementById("tela");
 	var c = tela.getContext("2d");	
-			
+	
+		
 			c.fillStyle="#00ff00"; 
 			c.fillRect(0,0,200,400); 
 			c.fillStyle="#ff0000";
@@ -11,5 +12,10 @@ function inicializar(){
 			c.fillStyle="blue";
 			c.beginPath();
 			c.arc(300,200,50,0 * Math.PI,2*Math.PI);
-			c.fill();			
+			c.fill();	
+
+			document.body.appendChild(tela);
 }
+
+
+
